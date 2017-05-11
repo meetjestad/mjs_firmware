dd25edc3510385b04b15360301d3ddd10e24f3a1
Matthijs Kooijman <matthijs@stdin.nl>
2017-05-11 13:01:04 +0200

    Print temperature and humidity at startup

--- mjs_firmware.ino
+++ mjs_firmware.ino
@@ -86,6 +86,15 @@ void setup() {
   // start communication to sensors
   htu.begin();
   gpsSerial.begin(9600);
+
+  if (DEBUG) {
+    temperature = htu.readTemperature();
+    humidity = htu.readHumidity();
+    Serial.print("Temperature: ");
+    Serial.println(temperature);
+    Serial.print("Humidity: ");
+    Serial.println(humidity);
+  }
 }
 
 void loop() {
