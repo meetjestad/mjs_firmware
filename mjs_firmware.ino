d74475aed137ae71d223e5bcfab15f23fc773fd5
Matthijs Kooijman <matthijs@stdin.nl>
2018-06-30 21:03:12 +0200

    Add code for reading a light sensor
    This is disabled by default and can be enabled by uncommenting the
    WITH_LUX define.

--- mjs_firmware.ino
+++ mjs_firmware.ino
@@ -42,6 +42,33 @@ const uint8_t FIRMWARE_VERSION = 1;
 float const BATTERY_DIVIDER_RATIO = 0.0;
 //float const BATTERY_DIVIDER_RATIO = (100.0 + 470.0) / 100.0;
 
+// Enable this define when a light sensor is attached
+//#define WITH_LUX
+
+// These values define the sensitivity and calibration of the PAR / Lux
+// measurement.
+// R12 Reference resistor for low light levels
+//  (nominal 100K in Platform Rev 2)
+// R11 Reference shunt resistor for high ligh levels
+//  (nominal 10K in platform Rev 2)
+// Value in Ohms
+float const R12 = 100000.0;
+// Value in Ohms
+float const R11 = 10000.0;
+
+// Reverse light current of the foto diode Ea at 1klx
+// uA @ 1000lx  eg 8.9 nA/lx
+// The Reverse dark current (max 30 nA ) is neglectable for our purpose
+float const light_current = 8.9;
+
+// R11 and R12 in parallel
+float const R11_R12 = (R12 * R11) / (R12 + R11);
+float const lx_conv_high = 1.0E6 / (R11_R12 * light_current * 1024.0);
+float const lx_conv_low = 1.0E6 / (R12 * light_current * 1024.0);
+
+// Value in mV (nominal @ 25ºC, Vcc=3.3V)
+// The temperature coefficient of the reference_voltage is neglected
+float const reference_voltage_internal = 1137.0;
 
 // setup GPS module
 uint8_t const GPS_PIN = 8;
@@ -55,10 +82,12 @@ HTU21D htu;
 float temperature;
 float humidity;
 uint16_t vcc = 0;
+uint16_t lux = 0;
 
 // define various pins
 uint8_t const SW_GND_PIN = 20;
 uint8_t const LED_PIN = 21;
+uint8_t const LUX_HIGH_PIN = 5;
 
 // setup timing variables
 uint32_t const UPDATE_INTERVAL = 900000;
@@ -70,7 +99,11 @@ uint32_t lastUpdateTime = 0;
 uint32_t updatesBeforeGpsUpdate = 0;
 gps_fix gps_data;
 
+#ifdef WITH_LUX
+uint8_t const LORA_PORT = 12;
+#else
 uint8_t const LORA_PORT = 11;
+#endif
 
 void setup() {
   // when in debugging mode start serial connection
@@ -86,6 +119,10 @@ void setup() {
   pinMode(SW_GND_PIN, OUTPUT);
   digitalWrite(SW_GND_PIN, LOW);
 
+  // This pin can be used in OUTPUT LOW mode to add an extra pulldown
+  // resistor, or in INPUT mode to keep it disconnected
+  pinMode(LUX_HIGH_PIN, INPUT);
+
   // blink 'hello'
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);
@@ -100,12 +137,17 @@ void setup() {
     temperature = htu.readTemperature();
     humidity = htu.readHumidity();
     vcc = readVcc();
+    lux = readLux();
     Serial.print(F("Temperature: "));
     Serial.println(temperature);
     Serial.print(F("Humidity: "));
     Serial.println(humidity);
     Serial.print(F("Vcc: "));
     Serial.println(vcc);
+#ifdef WITH_LUX
+    Serial.print(F("Lux: "));
+    Serial.println(lux);
+#endif // WITH_LUX
   }
 }
 
@@ -130,6 +172,9 @@ void loop() {
   temperature = htu.readTemperature();
   humidity = htu.readHumidity();
   vcc = readVcc();
+#ifdef WITH_LUX
+  lux = readLux();
+#endif // WITH_LUX
 
   if (DEBUG)
     dumpData();
@@ -212,6 +257,10 @@ void dumpData() {
   Serial.print(humidity, 1);
   Serial.print(F(", vcc="));
   Serial.print(vcc, 1);
+#ifdef WITH_LUX
+  Serial.print(F(", lux="));
+  Serial.print(lux);
+#endif // WITH_LUX
   Serial.println();
   Serial.flush();
 }
@@ -246,6 +295,9 @@ void getPosition()
 
 void queueData() {
   uint8_t length = (BATTERY_DIVIDER_RATIO ? 12 : 11);
+#ifdef WITH_LUX
+  length += 2;
+#endif
   uint8_t data[length];
   BitStream packet(data, sizeof(data));
 
@@ -275,6 +327,10 @@ void queueData() {
   // Encoded in units of 10mv, starting at 1V
   uint8_t vcc8 = (vcc - 1000) / 10;
   packet.append(vcc8, 8);
+
+#ifdef WITH_LUX
+  packet.append(lux, 16);
+#endif
 
   if (BATTERY_DIVIDER_RATIO) {
     analogReference(INTERNAL);
@@ -315,4 +371,57 @@ uint16_t readVcc()
 
   result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
   return result; // Vcc in millivolts
+}
+
+long readLux()
+{
+  long result = 0;
+  bool done = false;
+  int range = 0;
+
+  // Set the Reference Resistor to 100K
+  pinMode(LUX_HIGH_PIN, INPUT);
+  // Read the value of Analog input 2 against the internal reference
+  analogReference(INTERNAL);
+  uint16_t read_low = analogRead(A2);
+  // Check if read_low has an overflow
+  if (read_low < 1000)
+  {
+    result = long(lx_conv_low * reference_voltage_internal * read_low);
+    done = true;
+    range = 1;
+  } else {
+    // Set the Reference Resistor to 10K parallel with 100K = 9.091K
+    pinMode(LUX_HIGH_PIN, OUTPUT);
+    digitalWrite(LUX_HIGH_PIN, LOW);
+    // Read the value of Analog input 2 against the internal reference
+    analogReference(INTERNAL);
+    uint16_t read_high = analogRead(A2);
+    // Check if read_high has an overflow
+    if (read_high < 1000)
+    {
+      result = long(lx_conv_high * reference_voltage_internal * read_high);
+      range = 2;
+    } else {
+      // Set the Reference Resistor to 10K parallel with 100K = 9.091K
+      pinMode(LUX_HIGH_PIN, OUTPUT);
+      digitalWrite(LUX_HIGH_PIN, LOW);
+      // Read the value of Analog input 2 against the battery voltage
+      analogReference(DEFAULT);
+      uint16_t read_highest = analogRead(A2);
+      result = long(lx_conv_high * vcc * read_highest);
+      range = 3;
+    }
+  }
+
+  // Set the Reference Resistor to 100K to draw the least current
+  pinMode(LUX_HIGH_PIN, INPUT);
+  if (DEBUG)
+  {
+    Serial.print(F("Lux_reading : "));
+    Serial.print(result);
+    Serial.print(F(" lx, range="));
+    Serial.println(range);
+  }
+  return result;
 }
