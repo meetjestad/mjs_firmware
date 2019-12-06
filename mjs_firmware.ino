42d056cf379b5f5d539e4a5d2eedb1e4b562b749
Matthijs Kooijman <matthijs@stdin.nl>
2019-12-06 01:19:09 +0100

    Experiment with two luxmeters
    This code reads out two luxmeters and puts their calculated lux values,
    raw adc value and range in the extra data. The calculated lux value of
    the first meter is also put into the normal lux field.
    
    This code is written for an experiment comparing two covers for the
    luxmeter (pingpongball and 3mm teflon) to be mounted near an official
    light sensor in Bergen. The R11 value is lowered from 10k to 3.3k for
    some additional range.

--- mjs_firmware.ino
+++ mjs_firmware.ino
@@ -44,7 +44,7 @@ float const BATTERY_DIVIDER_RATIO = 0.0;
 //float const BATTERY_DIVIDER_RATIO = (100.0 + 470.0) / 100.0;
 
 // Enable this define when a light sensor is attached
-//#define WITH_LUX
+#define WITH_LUX
 
 // These values define the sensitivity and calibration of the PAR / Lux
 // measurement.
@@ -52,10 +52,10 @@ float const BATTERY_DIVIDER_RATIO = 0.0;
 //  (nominal 100K in Platform Rev 2)
 // R11 Reference shunt resistor for high ligh levels
 //  (nominal 10K in platform Rev 2)
-// Value in Ohms
-float const R12 = 100000.0;
-// Value in Ohms
-float const R11 = 10000.0;
+// Value in Ohms (nominally 100k, but measured 98.2 for Lux1 and 98.3 for Lux2)
+float const R12 = 98250.0;
+// Value in Ohms (nominally 3.3K, but measured 3.267 for Lux1 and 3.275 for Lux2)
+float const R11 = 3270.0;
 
 // Reverse light current of the foto diode Ea at 1klx
 // uA @ 1000lx  eg 8.9 nA/lx
@@ -81,19 +81,43 @@ HTU21D htu;
 float temperature;
 float humidity;
 uint16_t vcc = 0;
-#ifdef WITH_LUX
-uint32_t lux = 0;
-#endif
 int32_t lat24 = 0;
 int32_t lng24 = 0;
+
+struct luxmeter {
+  const uint8_t ADC_PIN;
+  const uint8_t CONTROL_PIN;
+  const uint8_t FACTOR;
+  uint32_t lux;
+  uint16_t adc_value;
+  uint8_t range;
+};
+
+luxmeter luxmeters[] = {
+  // Original lux meter plug
+  {
+    .ADC_PIN = A2,
+    .CONTROL_PIN = 5,
+    .FACTOR = 1,
+  },
+  // Soil moister sensor plug with added components
+  {
+    .ADC_PIN = A1,
+    .CONTROL_PIN = 6,
+    // The constants used in the calculation were determined based on
+    // the sensor used for lux1. For lux2, the enclosure has more
+    // attenuation, so this roughly corrects the measured lux value
+    // (this was not properly calibrated).
+    .FACTOR = 3,
+  },
+};
 
 // define various pins
 uint8_t const SW_GND_PIN = 20;
 uint8_t const LED_PIN = 21;
-uint8_t const LUX_HIGH_PIN = 5;
 
 // setup timing variables
-uint32_t const UPDATE_INTERVAL = 900000;
+uint32_t const UPDATE_INTERVAL = 60000;
 uint32_t const GPS_TIMEOUT = 120000;
 // Update GPS position after transmitting this many updates
 uint16_t const GPS_UPDATE_RATIO = 24*4;
@@ -132,7 +156,8 @@ void setup() {
 
   // This pin can be used in OUTPUT LOW mode to add an extra pulldown
   // resistor, or in INPUT mode to keep it disconnected
-  pinMode(LUX_HIGH_PIN, INPUT);
+  for (uint8_t i = 0; i < sizeof(luxmeters)/sizeof(*luxmeters); ++i)
+    pinMode(luxmeters[i].CONTROL_PIN, INPUT);
 
   // blink 'hello'
   pinMode(LED_PIN, OUTPUT);
@@ -149,7 +174,8 @@ void setup() {
     humidity = htu.readHumidity();
     vcc = readVcc();
 #ifdef WITH_LUX
-    lux = readLux();
+    readLux(0);
+    readLux(1);
 #endif
     Serial.print(F("Temperature: "));
     Serial.println(temperature);
@@ -158,8 +184,10 @@ void setup() {
     Serial.print(F("Vcc: "));
     Serial.println(vcc);
 #ifdef WITH_LUX
-    Serial.print(F("Lux: "));
-    Serial.println(lux);
+    Serial.print(F("Lux1: "));
+    Serial.println(luxmeters[0].lux);
+    Serial.print(F("Lux2: "));
+    Serial.println(luxmeters[1].lux);
 #endif // WITH_LUX
     if (BATTERY_DIVIDER_RATIO) {
       Serial.print(F("Battery Divider Ratio: "));
@@ -182,7 +210,7 @@ void loop() {
     // from other problems (lockups, downlink problems, etc).
     LMIC_setDrTxpow(DR_SF12, 14);
   } else {
-    LMIC_setDrTxpow(DR_SF9, 14);
+    LMIC_setDrTxpow(DR_SF7, 14);
   }
   updatesBeforeGpsUpdate--;
 
@@ -191,7 +219,8 @@ void loop() {
   humidity = htu.readHumidity();
   vcc = readVcc();
 #ifdef WITH_LUX
-  lux = readLux();
+  readLux(0);
+  readLux(1);
 #endif // WITH_LUX
 
   if (DEBUG)
@@ -276,8 +305,10 @@ void dumpData() {
   Serial.print(F(", vcc="));
   Serial.print(vcc, 1);
 #ifdef WITH_LUX
-  Serial.print(F(", lux="));
-  Serial.print(lux);
+  Serial.print(F(", lux1="));
+  Serial.print(luxmeters[0].lux);
+  Serial.print(F(", lux2="));
+  Serial.print(luxmeters[1].lux);
 #endif // WITH_LUX
   Serial.println();
   Serial.flush();
@@ -343,11 +374,9 @@ void queueData() {
   // of extra data bits to what you are actualy using. Additionally,
   // uncomment a bit of code further down that actually adds the data to
   // the packet, and also shows how the number of bits is counted.
-  /*
-  const uint8_t extra_bits = EXTRA_SIZE_BITS+10+EXTRA_SIZE_BITS+1;
+  const uint8_t extra_bits = (EXTRA_SIZE_BITS+18+EXTRA_SIZE_BITS+10+EXTRA_SIZE_BITS+2)*2;
   length += (extra_bits + 7)/8;
   flags |= FLAG_WITH_EXTRA;
-  */
 
   uint8_t data[length];
   BitStream packet(data, sizeof(data));
@@ -372,8 +401,10 @@ void queueData() {
   packet.append(vcc8, 8);
 
 #ifdef WITH_LUX
+// Do not append normal lux value
+
   // Chop off 2 bits to allow up to 256k lux (maximum solar power should be around 128k)
-  packet.append(lux >> 2, 16);
+  packet.append(luxmeters[0].lux >> 2, 16);
 #endif
 
   if (BATTERY_DIVIDER_RATIO) {
@@ -391,24 +422,23 @@ void queueData() {
   // 10-bit value followed by a 1 bit value. Each extra field is
   // transmitted as a 6-bit size field, followed by a (size+1)-bits value
   // field.
-  //
-  // Do not forget to uncomment the block around `extra_bits` a bit
-  // further up as well.
-  /*
-  // This uses some random values, replace these variables by your values.
-  uint16_t extra_adc = random(1 << 10);
-  uint16_t extra_bit = random(1 << 1);
-  // First field is 10 bits, subtract one to allow a size of 1-32
-  // (rather than 0-31)
-  packet.append(10-1, EXTRA_SIZE_BITS);
-  packet.append(extra_adc, 10);
-  // Second field is 1 bit
-  packet.append(1-1, EXTRA_SIZE_BITS);
-  packet.append(extra_bit, 1);
-  // Fill any remaining bits (from rounding up to whole bytes) with 1's,
+  for (uint8_t i = 0; i < sizeof(luxmeters)/sizeof(*luxmeters); ++i) {
+    // Send unscaled value (so 2 extra bits), since extra data is not
+    // processed in any way, so this makes it easier to process
+    packet.append(18-1, EXTRA_SIZE_BITS);
+    packet.append(luxmeters[i].lux, 18);
+
+    // Send raw 10-bit ADC value
+    packet.append(10-1, EXTRA_SIZE_BITS);
+    packet.append(luxmeters[i].adc_value, 10);
+
+    // Send scale (1-3)
+    packet.append(2-1, EXTRA_SIZE_BITS);
+    packet.append(luxmeters[i].range, 2);
+  }
+  // Fill any remaining bits (from rounding up to whole bytes) with 0's,
   // so they cannot be a valid field.
   packet.append(0xff, packet.free_bits());
-  */
 
   // Prepare upstream data transmission at the next possible time.
   LMIC_setTxData2(LORA_PORT, packet.data(), packet.byte_size(), 0);
@@ -455,28 +485,29 @@ uint16_t readVcc()
 }
 
 #ifdef WITH_LUX
-uint32_t readLux()
+void readLux(uint8_t idx)
 {
   uint32_t result = 0;
   uint8_t range = 0;
+  luxmeter& lux = luxmeters[idx];
 
   // Set the Reference Resistor to just R12
-  pinMode(LUX_HIGH_PIN, INPUT);
+  pinMode(lux.CONTROL_PIN, INPUT);
   // Read the value of Analog input 2 against the internal reference
   analogReference(INTERNAL);
   // Throw away the first reference, in case the internal reference
   // still needs to start up and stabilize (datasheet recommendation)
-  analogRead(A2);
-  uint16_t raw_adc = analogRead(A2);
+  analogRead(lux.ADC_PIN);
+  uint16_t raw_adc = analogRead(lux.ADC_PIN);
   // Check if read_low has an overflow
   if (raw_adc < 1000)
   {
-    result = uint32_t(lx_conv_low * reference_voltage_internal * raw_adc);
+    result = uint32_t(lx_conv_low * reference_voltage_internal * raw_adc * lux.FACTOR);
     range = 1;
   } else {
     // Set the Reference Resistor to R11 parallel with R12 for more range
-    pinMode(LUX_HIGH_PIN, OUTPUT);
-    digitalWrite(LUX_HIGH_PIN, LOW);
+    pinMode(lux.CONTROL_PIN, OUTPUT);
+    digitalWrite(lux.CONTROL_PIN, LOW);
     // An external capacitor can be added to charge the ADC internal 14pF
     // without dropping significant voltage, improving read values at low
     // values. If the capacitance is 1000x as big as the internal
@@ -487,36 +518,44 @@ uint32_t readLux()
     // capacitance.
     //
     // When switching from R12 to R11+R12, the capacitor has to charge
-    // through R11+R12, which has an RC time of 100nF x 9k = 900μs. To be
+    // through R11+R12, which has an RC time of 100nF x 3k = 300μs. To be
     // sure, we wait for 3ms.
     delay(3);
-    raw_adc = analogRead(A2);
+    raw_adc = analogRead(lux.ADC_PIN);
     // Check if read_high has an overflow
     if (raw_adc < 1000)
     {
-      result = uint32_t(lx_conv_high * reference_voltage_internal * raw_adc);
+      result = uint32_t(lx_conv_high * reference_voltage_internal * raw_adc * lux.FACTOR);
       range = 2;
     } else {
       // Read the value of Analog input 2 against VCC for more range
       analogReference(DEFAULT);
-      raw_adc = analogRead(A2);
-      result = uint32_t(lx_conv_high * vcc * raw_adc);
+      raw_adc = analogRead(lux.ADC_PIN);
+      result = uint32_t(lx_conv_high * vcc * raw_adc * lux.FACTOR);
       range = 3;
     }
   }
 
-  // Set the Reference Resistor to 100K to draw the least current
-  pinMode(LUX_HIGH_PIN, INPUT);
+  // Set the Reference Resistor to 100K to draw the least current and
+  // allow more than enough time for the capacitor to charge
+  pinMode(lux.CONTROL_PIN, INPUT);
   if (DEBUG)
   {
-    Serial.print(F("Lux_reading : "));
+    // Indent the second reading to make it easier to read the output
+    if (idx)
+      Serial.print(F("     "));
+    Serial.print(F("Lux"));
+    Serial.print(idx + 1);
+    Serial.print(F(": "));
     Serial.print(result);
     Serial.print(F(" lx, range="));
     Serial.print(range);
     Serial.print(F(", adc="));
     Serial.println(raw_adc);
   }
-  return result;
+  lux.lux = result;
+  lux.adc_value = raw_adc;
+  lux.range = range;
 }
 
 #endif
