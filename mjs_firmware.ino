meetjestad/mjs_firmware

commit fd785f94aa8f4787053e95d9d424b9f737666b86

Re-initialize GPS and GPS serial on every attempt
The GPS and GPS serial objects are now no longer global, but are
allocated inside `getPosition()` when needed. This ensures that they are
always cleanly initialized and additionally saves some global memory
(which is now only allocated when it is actually being used).

This change seems to improve the stability of GPS readouts (either this
commit, or the previous one does).

mjs_firmware.ino
@@ -72,8 +72,6 @@ float const reference_voltage_internal = 1137.0;
 
 // setup GPS module
 uint8_t const GPS_PIN = 8;
-SoftwareSerial gpsSerial(GPS_PIN, GPS_PIN);
-NMEAGPS gps;
 
 // Sensor object
 HTU21D htu;
@@ -135,7 +133,7 @@ void setup() {
 
   // start communication to sensors
   htu.begin();
-  gpsSerial.begin(9600);
+
 
   if (DEBUG) {
     temperature = htu.readTemperature();
@@ -278,7 +276,13 @@ void dumpData() {
 
 void getPosition()
 {
+  // Setup GPS
+  SoftwareSerial gpsSerial(GPS_PIN, GPS_PIN);
+  NMEAGPS gps;
+
+  gpsSerial.begin(9600);
   memset(&gps_data, 0, sizeof(gps_data));
+  gps.reset();
   gps.statistics.init();
 
   digitalWrite(SW_GND_PIN, HIGH);
@@ -308,6 +312,8 @@ void getPosition()
 
   if (gps.statistics.ok == 0)
     Serial.println(F("No GPS data received, check wiring"));
+
+  gpsSerial.end();
 }
 
 void queueData() {

commit c1606a70add6ca8b3f0da91eaace0d715832cc20

Move NMEAGPS object back to global scope
It turns out this object does not explicitly initialize all of its
members. If used as a global variable, they are initialized to zero, but
if used as a local variable, some members remain unitialized, taking
whatever value happens to be in memory.

This was observed to cause a buffer overflow when _firstFix was not
zero, leading to a fault because the htu object was overwritten and then
a reset.

Presumably this did not show up as a problem so far because the object
would be allocated in memory that usually or always contained values
that were ok, or maybe because AVR is less sensitive to e.g. null
pointer dereferencing (no hard fault and reset, but just reading random
values).

This is really a problem in the library, but we can work around it
easily by putting the variable back in global scope.

This problem was introduced in commit fd785f9 (Re-initialize GPS and GPS
serial on every attempt).

mjs_firmware.ino
@@ -127,6 +127,9 @@ float const lx_conv_low = 1.0E6 / (R12 * light_current * 1024.0);
 // Sensor object
 HTU21D htu;
 
+// GPS reader
+NMEAGPS gps;
+
 // Most recently read values
 float temperature;
 float humidity;
@@ -456,8 +459,6 @@ void getPosition()
   // Setup GPS
   SoftwareSerial GPS_SERIAL(GPS_PIN, GPS_PIN);
   #endif
-
-  NMEAGPS gps;
 
   GPS_SERIAL.begin(9600);
   memset(&gps_data, 0, sizeof(gps_data));